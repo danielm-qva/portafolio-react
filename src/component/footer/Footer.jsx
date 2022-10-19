208014965e6c1144af72457f30ddda2c94e89f0c
danielm-qva <jmastrapa96@gmail.com>
2022-10-19 12:38:56 -0400

    new change this is nav

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Container, Row , Col , Button } from 'react-bootstrap'
 
-
-
 export default function Footer() {
   const date = new Date();
   return (
-    <footer class="footer mt-auto py-3">
+    <footer className="footer mt-auto py-3">
     <Container>
     <Row className="justify-content-md-center mt-4" >
                 <Col >
@@ -14,22 +12,20 @@ export default function Footer() {
                  </Col>
                     <Col xs  md='auto'>
                           <Button variant='link' size="lg">
-                          <a  href="https://www.linkedin.com/in/daniel-alejandro-mastrapa-jimenez-26659b191/" target="_blank"> 
+                          <a  href="https://www.linkedin.com/in/daniel-alejandro-mastrapa-jimenez-26659b191/" target="_blank">
                                  <ion-icon name="logo-linkedin"></ion-icon>
                                 </a>
                           </Button>
                           <Button className='' variant='link' size="lg">
-                          <a  href="#" target="_blank"> 
+                          <a  href="#" target="_blank">
                           <ion-icon name="logo-github"></ion-icon>
                                 </a>
                           </Button>
                           <Button variant='link' size="lg">
-                          <a href='#' target="_blank"> 
+                          <a href='#' target="_blank">
                           <ion-icon name="logo-whatsapp"></ion-icon>
                                 </a>
                           </Button>
-                    
-        
                     </Col>
                     <Col xs lg='2'>
                         </Col>
